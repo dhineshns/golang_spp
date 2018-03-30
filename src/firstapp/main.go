179cb4c3981ab0c306bf3aa429a8e5c0c56a81df
dn046118 <dhinesh.nakkeerar@cerner.com>
2018-03-30 13:06:15 -0500

    6 done

--- src/firstapp/main.go
+++ src/firstapp/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 )
@@ -26,8 +27,45 @@ func askAndGreetAliceAndBob() {
 	}
 }
 
+func sumOf1ToN(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	return sum
+}
+
+func sumOf1ToNMulitplesOf3And5(n int) int {
+	sum := 0
+	for i := 0; i <= n; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
+
+func largetNum(arr []int) (int, error) {
+	largest := -1000000
+	for item := range arr {
+		if item > largest {
+			largest = item
+		}
+	}
+	err := func() error {
+		if largest == -1000000 {
+			return errors.New("errror")
+		} else {
+			return nil
+		}
+	}()
+	return largest, err
+}
 func main() {
-	helloWorld()
-	askAndGreet()
-	askAndGreetAliceAndBob()
+	// helloWorld()
+	// askAndGreet()
+	// askAndGreetAliceAndBob()
+	// fmt.Println(sumOf1ToN(3))
+	// fmt.Println(sumOf1ToNMulitplesOf3And5(9))
+	fmt.Println(largetNum([]int{1, 2, 3, 4, 5}))
 }
