5ac6b59f10cf0f4e9a6425df79e368d071e26800
dn046118 <dhinesh.nakkeerar@cerner.com>
2018-03-30 11:35:36 -0500

    3 done

--- src/firstapp/main.go
+++ src/firstapp/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func helloWorld() {
+	fmt.Printf("Hello")
+}
+
+func askAndGreet() {
+	fmt.Printf("Please enter your name: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	fmt.Printf("Hello " + input)
+}
+
+func askAndGreetAliceAndBob() {
+	fmt.Println("Please enter your name : ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	if input == "Alice" || input == "Bob" {
+		fmt.Println("Hello " + input)
+	}
+}
+
+func main() {
+	helloWorld()
+	askAndGreet()
+	askAndGreetAliceAndBob()
 }
